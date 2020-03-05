07129bac90641d77ed393601de6c7c0423ca1b67
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-03-05 15:39:08 -0500

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -1,5 +1,5 @@
   
-CREATE TABLE airlinesentiment ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
+CREATE TABLE airlinesentimentkudu ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
 host STRING,
 macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING, host_name STRING,
 node_id_0 INT, label_0 STRING, result_0 DOUBLE,
@@ -14,7 +14,7 @@ STORED AS KUDU
 TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
   
-CREATE TABLE airlinetweetsraw ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
+CREATE TABLE airlinetweetsrawkudu ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
 host STRING,
 macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING, host_name STRING,
 node_id_0 INT, label_0 STRING, result_0 DOUBLE,
@@ -27,3 +27,30 @@ PRIMARY KEY (uuid, `end`)
 PARTITION BY HASH PARTITIONS 4
 STORED AS KUDU
 TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+-- Create parquet tables for warehouse
+
+CREATE TABLE airlinetweetsraw ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
+host STRING,
+macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING, host_name STRING,
+node_id_0 INT, label_0 STRING, result_0 DOUBLE,
+node_id_1 INT, label_1 STRING, result_1 DOUBLE,
+node_id_2 INT, label_2 STRING, result_2 DOUBLE,
+node_id_3 INT, label_3 STRING, result_3 DOUBLE,
+node_id_4 INT, label_4 STRING, result_4 DOUBLE,
+PRIMARY KEY (uuid, `end`)
+)
+STORED AS PARQUET;
+
+CREATE TABLE airlinesentiment ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
+host STRING,
+macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING, host_name STRING,
+node_id_0 INT, label_0 STRING, result_0 DOUBLE,
+node_id_1 INT, label_1 STRING, result_1 DOUBLE,
+node_id_2 INT, label_2 STRING, result_2 DOUBLE,
+node_id_3 INT, label_3 STRING, result_3 DOUBLE,
+node_id_4 INT, label_4 STRING, result_4 DOUBLE,
+PRIMARY KEY (uuid, `end`)
+)
+STORED AS PARQUET;
