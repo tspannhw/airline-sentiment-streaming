f6a71303fbb8ecc3f738f4accfea4685703ea532
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-03-06 08:00:02 -0500

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -43,14 +43,14 @@ PRIMARY KEY (uuid, `end`)
 )
 STORED AS PARQUET;
 
-CREATE TABLE airlinesentiment ( uuid STRING,  `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING, te STRING, 
-host STRING,
-macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING, host_name STRING,
-node_id_0 INT, label_0 STRING, result_0 DOUBLE,
-node_id_1 INT, label_1 STRING, result_1 DOUBLE,
-node_id_2 INT, label_2 STRING, result_2 DOUBLE,
-node_id_3 INT, label_3 STRING, result_3 DOUBLE,
-node_id_4 INT, label_4 STRING, result_4 DOUBLE,
-PRIMARY KEY (uuid, `end`)
+CREATE TABLE airlinesentiment (
+tweetid STRING,
+annonym STRING,
+airlinesentiment STRING,
+label STRING,
+text STRING,
+tweet STRING,
+airline STRING,
+PRIMARY KEY (tweetid)
 )
 STORED AS PARQUET;
